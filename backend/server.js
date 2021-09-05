81b0ee7f03674478f5a570c48ee47d552d6d0400
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-05 21:19:20 +0530

    MongoDB connection added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,17 @@
 import express from 'express'
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
-const App = express();
+dotenv.config();
 
-App.get("/", (req, res) => {
+connectDB();
+
+const app = express();
+
+app.get("/", (req, res) => {
     res.send("Hey, you have made a get request!");
-})
+});
 
-App.listen(5000, () => {
+app.listen(5000, () => {
     console.log("Server is running at port 5000")
-})
+});
