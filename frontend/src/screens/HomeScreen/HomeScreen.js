6cb447a10f8db2ff78df8b30abda268e537d860f
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-08 23:36:51 +0530

    Added map and desc for HomeScreen

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -7,7 +7,7 @@ const HomeScreen = () => {
       <div className='top-banner'>
         <div className='content'>
           <h1>P Swaminathan</h1>
-          <p>Stam vendor, Trichy - 02</p>
+          <p>Stamp vendor, Trichy - 02</p>
         </div>
       </div>
 
@@ -39,6 +39,13 @@ const HomeScreen = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div id="info" className="map">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d244.92658458401914!2d78.69619460740755!3d10.824673709205687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1631121898380!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy"></iframe>
+      </div>
+      <div className="whatwedo">
+        <h3>"Give it a better start"</h3>
+        <p>We sell stamp papers for registration and other legal documentation purpose. We are doing this business for three generations and authorised by Government. Buying a land or giving debt is risky, It is always recommended to document it and we are here to help with that. For further information and consultation please contact us. </p>
       </div>
     </div>
   )
