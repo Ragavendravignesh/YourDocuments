59107b2bade952a16358a7dcb42b73e49ba70632
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-08 13:35:43 +0530

    Added homescreen with image overlay

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,13 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screens/HomeScreen/HomeScreen';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <main className='py-3'>
+      <main>
         <Container>
           <Route exact path='/' component={HomeScreen} />
         </Container>
---
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './homescreen.css'
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <div className='top-banner'>
+        <div className='content'>
+          <h1>P Swaminathan</h1>
+          <p>Stam vendor, Trichy - 08</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeScreen
