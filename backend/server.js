ff400e31838c665ed467c9ed97f5a5c40da4ba6c
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-11 14:12:39 +0530

    Added cors middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cors from 'cors';
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
@@ -13,6 +14,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/users', userRoutes);
 app.use('/api/indents', indentRoutes);
