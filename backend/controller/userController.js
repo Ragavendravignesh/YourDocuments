450ed7b2f2c878f2482a27baa187c8bc3496326c
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-11 14:12:17 +0530

    Controller change to fix issue

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -51,108 +51,116 @@ const updateProfile = asyncHandler(async (req, res) => {
       user.password = req.body.password
     }
 
-    const updatedProfile = await user.save();
+    const updatedProfile = await user.save()
 
     res.send({
       _id: updatedProfile._id,
       name: updatedProfile.name,
       email: updatedProfile.email,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     })
   } else {
     res.status(404)
     throw new Error('User not found')
   }
-});
+})
 
 // GET
 const getUsers = asyncHandler(async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({})
 
-  res.send({users});
-});
+  res.send({ users })
+})
 
 // POST
 const registerUser = asyncHandler(async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
 
-  const userExists = await user.findOne({ email })
+  const userExists = await User.findOne({ email })
 
-  if(userExists) {
-    res.status(401);
+  if (userExists) {
+    res.status(401)
 
-    throw new Error("User already exists");
+    throw new Error('User already exists')
   } else {
-    const user = await User.create({ name, email, password });
+    const user = await User.create({ name, email, password })
 
-    if(user) {
+    if (user) {
       res.send({
         _id: user._id,
         name: user.name,
         password: user.password,
         email: user.email,
-        token: generateToken(user._id)
-      });
+        token: generateToken(user._id),
+      })
     } else {
-      res.status(401);
-      throw new Error("Not able to create a user");
+      res.status(401)
+      throw new Error('Not able to create a user')
     }
   }
-});
+})
 
 //PUT
 const updateUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
 
-  if(user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
-    if(req.body.password) {
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
       user.password = req.body.password
     }
 
-    const updatedProfile = await user.save();
+    const updatedProfile = await user.save()
 
-    if(updatedProfile) {
+    if (updatedProfile) {
       res.send({
         _id: updatedProfile._id,
         name: updatedProfile.name,
         email: updatedProfile.email,
-        token: generateToken(updatedProfile._id)
+        token: generateToken(updatedProfile._id),
       })
     }
   } else {
-    res.status(401);
-    throw new Error('Sorry cannot able to fetch the user');
+    res.status(401)
+    throw new Error('Sorry cannot able to fetch the user')
   }
-});
+})
 
 //DELETE
 const deleteUser = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id)
 
-  if(user) {
-    await user.remove();
+  if (user) {
+    await user.remove()
 
-    res.json({ message: 'Data removed' });
+    res.json({ message: 'Data removed' })
   } else {
-    res.status(404);
-    throw new Error('User not found');
+    res.status(404)
+    throw new Error('User not found')
   }
-});
+})
 
 //Get user by id
 const getUserById = asyncHandler(async (req, res) => {
   try {
-    const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id)
 
-    res.json({user})
-  } catch(err) {
-    res.status(404);
+    res.json({ user })
+  } catch (err) {
+    res.status(404)
 
     throw new Error('User not found')
   }
-});
+})
 
-
-export { authUser, getProfile, updateProfile, getUsers, registerUser, updateUser, deleteUser, getUserById }
+export {
+  authUser,
+  getProfile,
+  updateProfile,
+  getUsers,
+  registerUser,
+  updateUser,
+  deleteUser,
+  getUserById,
+}
