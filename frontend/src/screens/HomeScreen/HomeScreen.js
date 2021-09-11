5539c56a9f280b2c4352235a57b1b35bde4cf3c8
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-11 14:16:01 +0530

    Screen changes

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -41,7 +41,7 @@ const HomeScreen = () => {
         </div>
       </div>
       <div id="info" className="map">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d244.92658458401914!2d78.69619460740755!3d10.824673709205687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1631121898380!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d244.92658458401914!2d78.69619460740755!3d10.824673709205687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1631121898380!5m2!1sen!2sin" width="100%" height="450" allowFullScreen="" loading="lazy"></iframe>
       </div>
       <div className="whatwedo">
         <h3>"Give it a better start"</h3>
--- frontend/src/screens/RegisterScreen/RegisterScreen.js
+++ frontend/src/screens/RegisterScreen/RegisterScreen.js
@@ -5,45 +5,54 @@ import { Form, Button, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import Loader from '../../components/Loader'
 import Message from '../../components/Message'
+import { register } from '../../actions/userActions'
 
 const RegisterScreen = ({ history }) => {
+  const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [message, setMessage] = useState('')
 
-  //   const dispatch = useDispatch()
+  const dispatch = useDispatch()
 
-  //   const userRegister = useSelector((state) => state.userRegister)
-  //   const { loading, error, userInfo } = userRegister
+  const userRegister = useSelector((state) => state.userRegister)
+  const { loading, error, userInfo } = userRegister
 
-  //   useEffect(() => {
-  //     if (userInfo) {
-  //       history.push('/')
-  //     }
-  //   }, [history, userInfo])
+  useEffect(() => {
+    if (userInfo) {
+      history.push('/')
+    }
+  }, [history, userInfo])
 
-  //   const submitHandler = (e) => {
-  //       e.preventDefault();
-
-  //       if(password !== confirmPassword ) {
-  //           setMessage("Passwords did not match")
-  //       }else {
-
-  //       }
-  //   }
   const submitHandler = (e) => {
     e.preventDefault()
+
+    if (password !== confirmPassword) {
+      setMessage('Passwords did not match')
+    } else {
+      dispatch(register(name, email, password))
+    }
   }
 
   return (
     <FormContainer>
       <h1>Sign up</h1>
 
-      {/* {loading && <Loader />} 
-      {error && <Message variant='danger'>{message}</Message>} */}
+      {loading && <Loader />}
+      {error && <Message variant="danger">{error}</Message> }
+      {message && <Message variant='danger'>{message}</Message>}
 
       <Form onSubmit={submitHandler}>
+        <Form.Group controlId='name'>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            placeholder='Enter name'
+            type='name'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </Form.Group>
         <Form.Group controlId='email'>
           <Form.Label>Email</Form.Label>
           <Form.Control
@@ -72,14 +81,16 @@ const RegisterScreen = ({ history }) => {
           />
         </Form.Group>
         <Form.Group>
-          <Button type='submit' variant='primary' style={{ marginTop: '2rem'}}>
+          <Button type='submit' variant='primary' style={{ marginTop: '2rem' }} className="form-btn">
             Register
           </Button>
         </Form.Group>
       </Form>
 
       <Row className='py-3'>
-        <span>Already have an account? <Link to='/login'>Login</Link></span>
+        <span>
+          Already have an account? <Link to='/login' className="form-link">Login</Link>
+        </span>
       </Row>
     </FormContainer>
   )
