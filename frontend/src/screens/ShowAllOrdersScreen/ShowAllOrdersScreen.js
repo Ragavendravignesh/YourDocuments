881c962369a7fb6ec361cfc079b3cbd189ddc320
Ragavendra S <ragavendrabest77@gmail.com>
2021-09-19 15:53:55 +0530

    Show my orders screen added

--- frontend/src/screens/ShowAllOrdersScreen/ShowAllOrdersScreen.js
+++ frontend/src/screens/ShowAllOrdersScreen/ShowAllOrdersScreen.js
@@ -20,27 +20,38 @@ const ShowAllOrdersScreen = () => {
     }
   }, [])
 
-  return <Container style={{marginTop:'4rem'}}>
-      { loading && <Loader/> }
-      { error && <Message variant='danger'>{error}</Message> }
-      { orders && orders.map((order, idx)=> (
-        <Card className='mt-2' key={idx}>
-          <Card.Title>{order.name}</Card.Title>
-          <Card.Subtitle className="mb-2 text-muted">{order.mobileno}</Card.Subtitle>
-          <Row>
-            <Col xs={5} md={3} lg={2}>
-            <div className="round">
-              <div className="content">{order.name.slice(0,1)}</div>
-            </div>
-            </Col>
-            <Col>
-              <h5>Amount: ₹{order.amount}</h5>
-              <p>Message: {order.message}</p>
-            </Col>
-          </Row>
-        </Card>
-      ))}
-  </Container>
+  return (
+    <Container style={{ marginTop: '4rem' }}>
+      {loading && <Loader />}
+      {error && <Message variant='danger'>{error}</Message>}
+      <Row className='justify-content-md-center'>
+        <Col lg={8} md={6}>
+          {orders &&
+            orders.map((order, idx) => (
+              <Card className='mt-2' key={idx}>
+                <Card.Title>{order.name}</Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  {order.mobileno}
+                </Card.Subtitle>
+                <Row>
+                  <Col xs={5} md={3} lg={2}>
+                    <div className='round'>
+                      <div className='first-letter'>
+                        {order.name.slice(0, 1)}
+                      </div>
+                    </div>
+                  </Col>
+                  <Col>
+                    <h5>Amount: ₹{order.amount}</h5>
+                    <p>Message: {order.message}</p>
+                  </Col>
+                </Row>
+              </Card>
+            ))}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ShowAllOrdersScreen
--- frontend/src/screens/ShowMyOrdersScreen/ShowMyOrdersScreen.js
+++ frontend/src/screens/ShowMyOrdersScreen/ShowMyOrdersScreen.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { getMyOrder } from '../../actions/orderActions'
+import { Card, Container, Row, Col } from 'react-bootstrap'
+import Loader from '../../components/Loader'
+import Message from '../../components/Message'
+import './showMyOrdersScreen.css'
 
 const ShowMyOrdersScreen = () => {
-    return (
-        <div style={{marginTop:'3rem'}}>  
-            <h1>Show My Orders</h1>
-        </div>
-    )
+  const dispatch = useDispatch()
+
+  const orderGetMine = useSelector((state) => state.orderGetMine)
+  const { loading, error, orders } = orderGetMine
+
+  useEffect(() => {
+    dispatch(getMyOrder())
+  }, [])
+
+  return (
+    <Container style={{ marginTop: '4rem' }}>
+      {loading && <Loader />}
+      {error && <Message variant='danger'>{error}</Message>}
+      <Row className='justify-content-md-center'>
+        <Col lg={8} md={6}>
+          {orders &&
+            orders.map((order, idx) => (
+              <Card className='order-card' key={idx}>
+                <Card.Text className='order-content'>
+                  Mobile : {order.mobileno}
+                  <br />
+                  Amount: {order.amount}
+                  <br />
+                  Message: {order.message}
+                </Card.Text>
+              </Card>
+            ))}
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default ShowMyOrdersScreen
